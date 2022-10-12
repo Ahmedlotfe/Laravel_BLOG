d800f5be7bc961341d3b0d76185641d4459902c4
MostafaLotfe <ahmedlotfe132@gmail.com>
2022-10-12 15:06:23 +0200

    Blade

---
+++ app/Models/Post.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+
+class Post
+{
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
+
+    public static function all()
+    {
+        return collect(File::files(resource_path("posts/")))
+            ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+            ->map(
+                fn ($document) => new Post(
+                    $title = $document->title,
+                    $excerpt = $document->excerpt,
+                    $date = $document->date,
+                    $body = $document->body(),
+                    $slug = $document->slug
+                )
+            )
+            ->sortByDesc('date');
+    }
+
+    public static function find($slug)
+    {
+        return static::all()->firstWhere('slug', $slug);
+    }
+
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+
+        if (!$post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+    }
+}
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
+
